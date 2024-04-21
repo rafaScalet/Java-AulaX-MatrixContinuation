953d6197b0ff9e32285b2a5d355cfec290d09db3
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 18:43:34 -0300

    completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,23 +6,35 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix = new int[3][3];
         Scanner in = new Scanner(System.in);
+        int value;
 
         System.out.println("Provide the numbers: ");
-        for(int row = 0; row < matrix[0].length; row++){
-            for(int col = 0; col < matrix[0].length; col++){
+        for (int row = 0; row < matrix[0].length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
                 matrix[row][col] = in.nextInt();
             }
         }
+
+        System.out.print("Search a number: ");
+        value = in.nextInt();
         in.close();
 
-        System.out.println("Main Diagonal: ");
-        for(int indx = 0; indx < matrix[0].length; indx++){
-            System.out.println(matrix[indx][indx]);
+        for (int row = 0; row < matrix[0].length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == value) {
+                    System.out.println("The number " + value + " is in the matrix at position: [" + row + "," + col + "]");
+                }
+            }
         }
 
-        System.out.println("Secundary Diagonal: ");
-        for(int indx = 0; indx < matrix[0].length; indx++){
-            System.out.println(matrix[indx][(matrix[0].length - 1) - indx]);
+        for (int indx = 0; indx < matrix[0].length; indx++) {
+            if (matrix[indx][indx] == value && matrix[indx][(matrix[0].length - 1) - indx] == value) {
+                System.out.println("The number is on both Diagonals");
+            } else if (matrix[indx][(matrix[0].length - 1) - indx] == value) {
+                System.out.println("The number is on the Secondary Diagonal");
+            } else if (matrix[indx][indx] == value) {
+                System.out.println("The number is on the Main Diagonal");
+            }
         }
     }
 }
